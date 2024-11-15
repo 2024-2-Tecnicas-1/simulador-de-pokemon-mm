d22ab66faf7f8f9102687280fc4188666922de86
yu <u8i0po+@h.com>
2024-11-14 20:16:19 -0500

    se cambio hola

--- src/main/java/com/mycompany/proyectopokemon1/ProyectoPokemon1.java
+++ src/main/java/com/mycompany/proyectopokemon1/ProyectoPokemon1.java
@@ -3,7 +3,7 @@ package com.mycompany.proyectopokemon1;
 public class ProyectoPokemon1 {
     public static void main(String[] args) {
         
-       //hola
+       //chao
         Entrenador entrenador = new Entrenador("Ash");
         Menu menu = new Menu(entrenador);
         menu.mostrarMenu();
