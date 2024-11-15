548875a0b5695317808f980b85fe7afc00b057b6
yu <u8i0po+@h.com>
2024-11-14 20:14:09 -0500

    correccion

--- src/main/java/com/mycompany/proyectopokemon1/ProyectoPokemon1.java
+++ src/main/java/com/mycompany/proyectopokemon1/ProyectoPokemon1.java
@@ -2,6 +2,8 @@ package com.mycompany.proyectopokemon1;
  
 public class ProyectoPokemon1 {
     public static void main(String[] args) {
+        
+       //hola
         Entrenador entrenador = new Entrenador("Ash");
         Menu menu = new Menu(entrenador);
         menu.mostrarMenu();
